1b0998464abfabb383a334ca98f5b65094f39a2a
Nico Lerche <nico.lerche@stud.uni-goettingen.de>
2026-06-11 13:51:28 +0200

    Assignment-Funktion entfernt, import Statement an den Anfang der Datei gesetzt, Parameter von mergesort umbenannt, mergeSort in merge_sort umbenannt, Basisfall zusammengefasst und gekürzt, l,r,i Indizes umbenannt, Programm in eine main-Funktion geschrieben

--- mergesort.py
+++ mergesort.py
@@ -1,51 +1,68 @@
-def ASSIGNMENT(new_list, i, old_list, j):
-    new_list[i] = old_list[j]
-
-
-def mergeSort(list_to_sort_by_merge):
-    if (
-        len(list_to_sort_by_merge) > 1
-        and not len(list_to_sort_by_merge) < 1
-        and len(list_to_sort_by_merge) != 0
-    ):
-        mid = len(list_to_sort_by_merge) // 2
-        left = list_to_sort_by_merge[:mid]
-        right = list_to_sort_by_merge[mid:]
-
-        mergeSort(left)
-        mergeSort(right)
-
-        l = 0
-        r = 0
-        i = 0
-
-        while l < len(left) and r < len(right):
-            if left[l] <= right[r]:
-                ASSIGNMENT(new_list=list_to_sort_by_merge, i=i, old_list=left, j=l)
-                l += 1
-            else:
-                ASSIGNMENT(new_list=list_to_sort_by_merge, i=i, old_list=right, j=r)
-                r += 1
-            i += 1
-
-        while l < len(left):
-            list_to_sort_by_merge[i] = left[l]
-            l += 1
-            i += 1
-
-        while r < len(right):
-            list_to_sort_by_merge[i] = right[r]
-            r += 1
-            i += 1
-
-
+# Änderung: import Statement nach oben für Übersicht im Code
 import matplotlib.pyplot as plt
 
-my_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
-x = range(len(my_list))
-plt.plot(x, my_list)
-plt.show()
-mergeSort(my_list)
-x = range(len(my_list))
-plt.plot(x, my_list)
-plt.show()
+# Änderung: ASSIGNMENT ist überflüssig, da es die Zuweisung nur unnötig kompliziert macht
+
+# Änderung: Parameter in unsorted_list umbenannt: gleiche Bedeutung, aber weniger Zeichen und einfacher zu lesen als list_to_be_sorted_by_merge
+# Änderung: umbenannt in merge_sort --> alles klein und mit Unterstrich zwischen den Worten
+def merge_sort(unsorted_list):
+    # Änderung: Abfrage zur Länge der Liste in eine Anfrage gebündelt + return, wenn Länge <= 1, um nicht die eigentlichen Anweisungen der Funktion unter Bedingung zu stellen --> übersichtlicher
+    # Basisfall: übergebene Liste hat 1 oder kein Argument und muss/kann nicht mehr geteilt werden
+    if (len(unsorted_list) <= 1):
+        return
+    
+    # Aufteilung der Listen in n/2 links und ab n/2 rechts
+    mid = len(unsorted_list) // 2
+    left = unsorted_list[:mid]
+    right = unsorted_list[mid:]
+
+    # Rekursiver Aufruf
+    merge_sort(left)
+    merge_sort(right)
+
+    # Index Initialisierung für links, rechts und zu sortierender Liste
+    index_left = 0
+    index_right = 0
+    index_to_place = 0
+
+    # Änderung: da die Länge mehrfach für Abfragen benötigt wird, kann man sie auch in einer Variable Speichern, um Funktionsaufrufe zu verringern
+    length_left = len(left)
+    length_right = len(right)
+
+    # Entlang der sortierten linken und rechten Liste gehen und mittels Vergleich nacheinander in die Ausgangsliste kopieren (inplace Operation sozusagen) 
+    while index_left < length_left and index_right < length_right:
+        if left[index_left] <= right[index_right]:
+            unsorted_list[index_to_place] = left[index_left] # Änderung: hier wurde einfache Zuweisung statt Assignment hinzugefügt
+            index_left += 1
+        else:
+            unsorted_list[index_to_place] = right[index_right] # Änderung: hier wurde einfache Zuweisung statt Assignment hinzugefügt
+            index_right += 1
+        index_to_place += 1
+
+    # Restliche Elemente der Liste kopieren, die länger als die andere gewesen ist  
+    while index_left < length_left:
+        unsorted_list[index_to_place] = left[index_left]
+        index_left += 1
+        index_to_place += 1
+
+    while index_right < length_right:
+        unsorted_list[index_to_place] = right[index_right]
+        index_right += 1
+        index_to_place += 1
+
+# Änderung: Programm in einer main-Funktion bündeln, um besser zwischen den Funktionen zu trennen
+def main():
+    my_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+    print("unsortierte Liste: " + str(my_list)) # Keine direkte Änderung, sondern eher für die Kontrolle, dass der Code von mergeSort noch dasselbe macht
+    x = range(len(my_list))
+    plt.plot(x, my_list)
+    plt.show()
+    merge_sort(my_list)
+    print("sortierte Liste: " + str(my_list)) # Keine direkte Änderung, sondern eher für die Kontrolle, dass der Code von mergeSort noch dasselbe macht
+    x = range(len(my_list))
+    plt.plot(x, my_list)
+    plt.show()
+
+# Notwendig, damit beim Datei-Start auch die main ausgeführt wird
+if __name__ == "__main__":
+    main()
